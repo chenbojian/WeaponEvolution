278b9248a785e13f396e9fe57a6355fac57dface
Bojian Chen <bjchen@thoughtworks.com>
2018-08-27 10:50:40 +0530

    add DuelCenter to make two players duel

--- test/test_question1.py
+++ test/test_question1.py
@@ -1,6 +1,17 @@
-from weapon_evolution import Player as Player
+from weapon_evolution import Player as Player, DuelCenter
 
 
-def test_player_have_life_value():
-    player = Player(100)
-    assert player.life_value == 100
+def test_player_can_attack_another_player():
+    zhang = Player(name='zhang', life_value=100, attack_value=3)
+    li = Player(name='li', life_value=100, attack_value=1)
+    zhang.attack(li)
+    assert li.life_value == 97
+
+
+def test_duel_center_can_arrange_duel():
+    zhang = Player(name='zhang', life_value=100, attack_value=3)
+    li = Player(name='li', life_value=100, attack_value=1)
+    duel_center = DuelCenter(player1=zhang, player2=li)
+    duel_center.start_duel()
+    assert zhang.is_alive() == True
+    assert li.is_alive() == False
--- weapon_evolution/__init__.py
+++ weapon_evolution/__init__.py
@@ -1,4 +1,22 @@
 class Player:
-    def __init__(self, life_value):
+    def __init__(self, name, life_value, attack_value):
+        self.name = name
         self.life_value = life_value
+        self.attack_value = attack_value
 
+    def attack(self, enemy_player: 'Player'):
+        enemy_player.life_value = enemy_player.life_value - self.attack_value
+
+    def is_alive(self):
+        return self.life_value > 0
+
+
+class DuelCenter:
+    def __init__(self, player1: Player, player2: Player):
+        self.player1 = player1
+        self.player2 = player2
+
+    def start_duel(self):
+        while self.player1.is_alive() and self.player2.is_alive():
+            self.player1.attack(self.player2)
+            self.player2.attack(self.player1)
